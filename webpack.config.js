71e5a3c89ca7f90c44ecc73c9e2fc14bc15e8c0e
Dennis Hsu <denniseh7@gmail.com>
2023-03-16 11:24:55 -0700

    added initial webpack config file that can be tweaked

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "/client/src/index.jsx"),
+  output: {
+    path: path.join(__dirname, "/client/dist"),
+    filename: "bundle.js",
+  },
+  devtool: "source-map",
+
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        AUTH_SECRET: JSON.stringify(process.env.AUTH_SECRET),
+      },
+    }),
+  ],
+};
