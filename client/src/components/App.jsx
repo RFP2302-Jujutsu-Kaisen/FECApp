25b67f950db122394ebe556f861e2ce297d1a3b0
Donn Neufeld <donn.neufeld@gmail.com>
2023-03-20 16:53:04 -0700

    base dir set up for ratingsandreviews

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import RatingsAndReviews from './ratings-and-reviews';
 
 export default function App() {
   return (
+    <div>
     <p>Hello World</p>
+      <RatingsAndReviews />
+    </div>
   );
 }
---
+++ client/src/components/ratings-and-reviews/RatingsAndReviews.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const RatingsAndReviews = () => {
+
+  return (
+    <div className="RatingsAndReviews" >
+      <h2>Component: Ratings and Reviews</h2>
+    </div>
+
+  )
+};
+
+
+export default RatingsAndReviews;
