3662d96a4a9bf799ac7198897732baabcaf328dd
Donn Neufeld <donn.neufeld@gmail.com>
2023-03-20 17:34:37 -0700

    passing props to sub-components and fighting with linter

--- client/src/components/ratings-and-reviews/RatingsAndReviews.jsx
+++ client/src/components/ratings-and-reviews/RatingsAndReviews.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import { React, useState } from 'react';
+import RatingSummary from './rr-components/RatingSummary';
+import data from './assets/localData';
+
+
 
 
 
 const RatingsAndReviews = () => {
+  const [summaryData, setSummaryData] = useState(data);
 
   return (
     <div className="RatingsAndReviews" >
       <h2>Component: Ratings and Reviews</h2>
+      <RatingSummary data={summaryData} />
     </div>
-
   )
-};
+}
 
 
 export default RatingsAndReviews;
---
+++ client/src/components/ratings-and-reviews/rr-components/RatingSummary/RatingSummary.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const RatingSummary = ({ summaryData }) => {
+  console.log(summaryData);
+
+  return (
+    <div>
+      <h3>Ratings Summary</h3>
+    </div>
+  )
+}
+
+export default RatingSummary;
