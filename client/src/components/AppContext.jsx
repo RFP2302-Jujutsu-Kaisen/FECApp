e23d70698cd6cb1ff8b80cae16e02129541aa29c
Elliott <tung.elliott@gmail.com>
2023-03-21 10:31:02 -0700

    first pass at context/reducer without running code

--- client/src/components/AppContext.jsx
+++ client/src/components/AppContext.jsx
@@ -1,2 +1,63 @@
 // TODO: store Product ID Context
 // TODO: store Product Average Rating
+import React, {
+  createContext,
+  useContext,
+  useReducer,
+  useEffect,
+  useMemo,
+  useCallback,
+} from 'react';
+import axios from 'axios';
+import appReducer from './AppReducer';
+
+// Create the AppContext
+const AppContext = createContext();
+
+// Custom hook to use the context
+export function useAppContext() {
+  return useContext(AppContext);
+}
+
+// Initial state of the context
+const initialState = {
+  productId: null,
+  productAvgRating: -1,
+};
+
+// Context provider component
+export function AppContextProvider({ children }) {
+  // Using a reducer to manage state and dispatch
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
+  // Fetch product data when the component mounts
+  useEffect(() => {
+    axios
+      .get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344')
+      .then((res) => {
+        const { productId } = res.data;
+        dispatch({ type: 'SET_PRODUCT_ID', payload: productId });
+      })
+      .catch((err) => {
+        console.error('Error fetching product data: ', err);
+      });
+  }, []);
+
+  // Memoized function to set product ID
+  const setProductId = useCallback(
+    (productId) => dispatch({ type: 'SET_PRODUCT_ID', payload: productId }),
+    [dispatch],
+  );
+
+  // Memoized value object to prevent unnecessary re-renders
+  const value = useMemo(
+    () => ({
+      state, // Current state object
+      setProductId, // Memoized function to set product ID
+    }),
+    [state, setProductId], // Update value only when state or setProductId changes
+  );
+
+  // Return the provider with the memoized value object
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+}
--- client/src/components/AppReducer.jsx
+++ client/src/components/AppReducer.jsx
@@ -1,1 +1,19 @@
 // TODO: Reducer for Product ID Changes
+export default function appReducer(state, action) {
+  // Use a switch statement to handle different action types
+  switch (action.type) {
+    // When action type is 'SET_PRODUCT_ID'
+    case 'SET_PRODUCT_ID':
+      // Return a new state object with the updated productId
+      return { ...state, productId: action.payload };
+
+    // When action type is 'SET_PRODUCT_AVG_RATING'
+    case 'SET_PRODUCT_AVG_RATING':
+      // Return a new state object with the updated productAvgRating
+      return { ...state, productAvgRating: action.payload };
+
+    // For any other action type, return the current state
+    default:
+      return state;
+  }
+}
