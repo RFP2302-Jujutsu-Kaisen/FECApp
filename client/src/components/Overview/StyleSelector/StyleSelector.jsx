7c1073365e9bb39d26b4e31c4197ca3a06e2b4ed
Dennis Hsu <denniseh7@gmail.com>
2023-03-23 00:21:49 -0700

    implemented Style Selector with switching styles

--- client/src/components/Overview/StyleSelector/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector/StyleSelector.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import StyleThumbnail from './StyleThumbnail';
 
-export default function StyleSelector() {
+export default function StyleSelector({ styles, setStyles }) {
+  const setSelected = (index) => {
+    setStyles([styles[0], index]);
+  };
+
   return (
-    <h3>StyleSelector</h3>
+    <div>
+      <h3>StyleSelector</h3>
+      <ul>
+        {styles[0].map((style, index) => (
+          <StyleThumbnail
+            key={style.style_id}
+            style={style}
+            selected={index === styles[1]}
+            setSelected={setSelected}
+            index={index}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
---
+++ client/src/components/Overview/StyleSelector/StyleThumbnail.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default function StyleThumbnail({ style, selected, setSelected, index,
+}) {
+  const imgClickHandler = (event) => {
+    if (!selected) {
+      setSelected(index);
+    }
+    event.target.blur(); // stop focusing on input/image
+    event.preventDefault();
+  };
+
+  // add check mark if selected
+  if (selected) {
+    return (
+      <li>
+        <span>checked</span>
+        <input
+          type="image"
+          src={style.photos[0].thumbnail_url}
+          alt={style.style_id}
+          height="80"
+          width="80"
+          onClick={imgClickHandler}
+          onKeyDown={imgClickHandler}
+        />
+        <span>{style.style_id}</span>
+      </li>
+    );
+  }
+
+  return (
+    <li>
+      <input
+        type="image"
+        src={style.photos[0].thumbnail_url}
+        alt={style.style_id}
+        height="80"
+        width="80"
+        onClick={imgClickHandler}
+        onKeyDown={imgClickHandler}
+      />
+      <span>{style.style_id}</span>
+    </li>
+  );
+}
