41dfae8d6bef1f7ae386521020cfe52de39d609c
Dennis Hsu <denniseh7@gmail.com>
2023-03-22 22:15:55 -0700

    implemented Overview component with live data

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ImageGallery, ProductInfo, StyleSelector, AddToCart, Description,
 } from '.';
 
+import Parse from './Parse';
+
 import eData from './exampleData';
 
+import { useAppContext } from '../AppContext';
+
+const prodId = '40344';
 export default function Overview() {
+  //context, states
+  const product = useAppContext();
+  const [prod, setProd] = useState({});
+  const [styles, setStyles] = useState([[], 0]);
+  // const [style, setStyle] = useState({});
+
+  useEffect(() => {
+    if (product.state.productId) {
+      Parse.getProd(product.state.productId, setProd);
+      Parse.getStyles(product.state.productId, setStyles);
+    }
+  }, [product.state.productId]);
+
+  // useEffect(() => {
+  //   Parse.getProd(prodId, setProd);
+  // }, []);
+
+  // useEffect(() => {
+
+  // }, [prod]);
+
+  // console.log('edata styles', eData.styles);
   return (
     <div>
       <h2>Overview</h2>
       <ImageGallery />
-      <ProductInfo productInfo={eData} />
+      <ProductInfo prod={prod} style={styles[0][styles[1]] || {}} />
       <StyleSelector />
       <AddToCart />
       <Description />
