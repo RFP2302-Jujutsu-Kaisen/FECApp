570630b3d638d1896a9624720de2341cbf7bfa2b
Elliott <tung.elliott@gmail.com>
2023-03-28 22:36:26 -0700

    build initial test

--- client/src/components/QAModule/QuestionsAndAnswers.test.jsx
+++ client/src/components/QAModule/QuestionsAndAnswers.test.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { AppContextProvider } from '../AppContext';
 import QuestionsAndAnswers from './QuestionsAndAnswers';
+
+jest.mock('axios');
+axios.get.mockResolvedValue({
+  data: {
+    results: [
+      {
+        question_id: 1,
+        question_body: 'Example question?',
+        question_helpfulness: 10,
+      },
+    ],
+  },
+});
 
 describe('QuestionsAndAnswers', () => {
   beforeEach(() => {
@@ -15,52 +27,7 @@ describe('QuestionsAndAnswers', () => {
     );
   });
 
-  test('renders the component', () => {
-    const heading = screen.getByText(/QUESTIONS & ANSWERS/i);
-    expect(heading).toBeInTheDocument();
-  });
-
-  test('renders the SearchBar component', () => {
-    const searchBar = screen.getByPlaceholderText('HAVE A QUESTION? SEARCH FOR ANSWERS...');
-    expect(searchBar).toBeInTheDocument();
-  });
-
-  test('renders the QuestionsList component', () => {
-    const questionsList = screen.getByTestId('questions-list');
-    expect(questionsList).toBeInTheDocument();
-  });
-
-  test('filters questions when searching', async () => {
-    // Assuming you have a function that mocks the API call and returns questions data
-    const { questions } = mockApiData();
-
-    jest.spyOn(axios, 'get').mockResolvedValue({ data: { results: questions } });
-
-    render(
-      <AppContextProvider>
-        <QuestionsAndAnswers />
-      </AppContextProvider>,
-    );
-
-    await screen.findByText(questions[0].question_body);
-
-    const searchTerm = 'example';
-    userEvent.type(screen.getByPlaceholderText('HAVE A QUESTION? SEARCH FOR ANSWERS...'), searchTerm);
-
-    expect(screen.getByText(questions[1].question_body)).toBeInTheDocument();
-    expect(screen.queryByText(questions[0].question_body)).not.toBeInTheDocument();
-  });
-
-  test('opens and closes the "Add a Question" modal', () => {
-    const addQuestionButton = screen.getByText('ADD A QUESTION +');
-    userEvent.click(addQuestionButton);
-
-    const modalHeading = screen.getByText('Ask Your Question');
-    expect(modalHeading).toBeInTheDocument();
-
-    const closeButton = screen.getByText('×');
-    userEvent.click(closeButton);
-
-    expect(modalHeading).not.toBeInTheDocument();
+  test('renders without crashing', () => {
+    expect(screen.getByText(/questions & answers/i)).toBeInTheDocument();
   });
 });
